bb38bf2a891bd93981bd5a395d9bcc2741881e2f
JeffersonAhimar <jeffersonahimar@outlook.com>
2023-06-03 13:40:08 -0500

    documentation with swagger

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "My first app with API"
+app.version = "0.0.1"
 
-@app.get('/')
+
+
+@app.get('/', tags=['home'])
 def message():
     return "Hello world!"

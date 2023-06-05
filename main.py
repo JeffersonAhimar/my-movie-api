e2edaa33edabe39f9cb9edf7af643814eed64883
JeffersonAhimar <jeffersonahimar@outlook.com>
2023-06-04 23:11:36 -0500

    user router added

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, JSONResponse
-from pydantic import BaseModel
-from jwt_manager import create_token
+from fastapi.responses import HTMLResponse
 from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
+from routers.user import user_router
 
 app = FastAPI()
 app.title = "My first app with API"
@@ -12,39 +11,10 @@ app.version = "0.0.2"
 
 app.add_middleware(ErrorHandler)
 app.include_router(movie_router)
+app.include_router(user_router)
 
 Base.metadata.create_all(bind=engine)
-
-class User(BaseModel):
-    email:str
-    password:str
-
-movies = [
-    {
-        'id': 1,
-        'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
-        'rating': 7.8,
-        'category': 'Acción'    
-    },
-    {
-        'id': 2,
-        'title': 'Avatar',
-        'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
-        'rating': 7.8,
-        'category': 'Aventura'    
-    },
-]
 
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse('<h1>Hello world</h1>')
-
-# LOGIN
-@app.post('/login', tags=['auth'])
-def login(user: User):
-    if user.email == "admin@gmail.com" and user.password == "admin":
-        token: str = create_token(user.dict())
-        return JSONResponse(status_code=200, content=token)
